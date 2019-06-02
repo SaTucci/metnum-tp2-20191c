f895cde1f0e65ad7b60d4b2d7b5b3857f54852ed
tejo240 <pboitier@dc.uba.ar>
2019-06-02 20:07:29 -0300

    PCA arreglado. Deberia andar bien, pero lo quiero testear un poco mas

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,37 @@
 #include "eigen.cpp"
 #include "knn.cpp"
+#include "pca.cpp"
 #include <Eigen/Dense>
 #include <iostream>
+//#include <pybind11/pybind11.h>
+//#include <pybind11/eigen.h>
+
+
+//namespace py=pybind11;
 
 int main(){
 
     
-    Matrix X = Matrix(4,4);
+    Matrix X = Matrix(5,4);
     Matrix y = Matrix(1,4);
     Matrix predict = Matrix(1,4);
-    X <<    100,200,300,400,
-            2000,3000,4000,5000,
-            3,4,5,6,
-            2000,30000,400,500000;
+    X <<    1,2,3,4,
+            105,107,113,125,
+            14,225,6,9,
+            17,23,15,94,
+            1784,12,19,135;
     y << 1, 8, 3, 7;
     predict << 2,3,4,5;
+
+    SparseMatrix XSparse = X.sparseView();
+    cout << XSparse << endl;
     // cout << X << endl << y << endl << predict << endl;
     // auto res = get_first_eigenvalues(m1, 3, 10000, 0.00001);
-    KNNClassifier knn = KNNClassifier(2);
-    knn.fit(X,y);
-    cout << knn.predict(predict) << endl;
+    //KNNClassifier knn = KNNClassifier(2);
+    //knn.fit(X,y);
+    PCA pca(3);
+    pca.fit(XSparse,100000,1e-16);
+    //cout << knn.predict(predict) << endl;
     
 
     // cout << "result: " << res.first <<  ", " << endl << res.second << endl;
--- src/pca.cpp
+++ src/pca.cpp
@@ -2,36 +2,34 @@
 #include "pca.h"
 #include "eigen.h"
 #include <cmath>
+#include <stdexcept>
 using namespace std;
-
-
 PCA::PCA(unsigned int n_components)
 {
-	max_alfa = n_components;     
+	max_alfa = n_components;  
 }
 
 void PCA::fit(SparseMatrix Y, unsigned int num_iter, double epsilon)
 {
+	//py::print("EMPIEZO FIT");
 	M = MatrixXd(Y);
 	//M = Y;
 	//Calculo mu, el vector de promedios de las variables de cada medicion
 	//(O sea el promedio para cada fila de la matriz)
-	Matrix mu(M.cols(),1);
-	for(int i = 0; i < M.cols(); i++){
-        //el i itera por columna pero accede por fila! esto puede causar problemas
-		mu(i) = M.row(i).sum()/M.rows();
+	Matrix mu(1,M.cols());
+	for(int i = 0; i < M.rows(); i++){
+		mu += M.row(i);
 	}
-	cout << mu << endl; 
+	mu = mu/M.rows();
 	Matrix X(M.rows(),M.cols());
-	for(int i = 0; i < M.cols(); i++){
+	for(int i = 0; i < M.rows(); i++){
         //el i itera por columna pero accede por fila! esto puede causar problemas
-		X.row(i) = (M.row(i) - mu.transpose()).transpose() / sqrt(M.rows() - 1);
-	} 
-	
+		X.row(i) = (M.row(i) - mu) / sqrt(M.rows() - 1);
+	}
 	Matrix Cov = X.transpose() * X;
-	cout << Cov << endl;
 	auto eig = get_first_eigenvalues(Cov, max_alfa, num_iter, epsilon);
 	V = eig.second;
+	cout << V << endl; 
 }
 
 
@@ -41,5 +39,6 @@ MatrixXd PCA::transform(SparseMatrix Y, int alfa)
 	Matrix denseY = MatrixXd(Y);
 	Matrix trimmedV = V(Eigen::all, Eigen::seq(0,alfa - 1)); 
 	Matrix res = denseY* trimmedV;
+	cout << res << endl;
 	return res;
 }
